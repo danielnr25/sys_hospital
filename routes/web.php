9a0b3bf3659b2443965ee6fa469882ba25ca7adc
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2023-08-16 11:51:25 -0500

    feat:finish

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return redirect('/login'); // view('welcome');
 });
@@ -10,24 +13,24 @@ Route::get('/home', 'HomeController@index')->name('home'); // {{ route('home') }
 
 Route::middleware(['auth', 'admin'])->namespace('Admin')->group(function () {
     // Specialty
-	Route::get('/specialties', 'SpecialtyController@index');
-	Route::get('/specialties/create', 'SpecialtyController@create'); // form registro
-	Route::get('/specialties/{specialty}/edit', 'SpecialtyController@edit');
-	Route::post('/specialties', 'SpecialtyController@store'); // envío del form
-	Route::put('/specialties/{specialty}', 'SpecialtyController@update');
-	Route::delete('/specialties/{specialty}', 'SpecialtyController@destroy');
-	// Doctors
-	Route::resource('doctors', 'DoctorController');
-	// Patients
-	Route::resource('patients', 'PatientController');
+    Route::get('/specialties', 'SpecialtyController@index');
+    Route::get('/specialties/create', 'SpecialtyController@create'); // form registro
+    Route::get('/specialties/{specialty}/edit', 'SpecialtyController@edit');
+    Route::post('/specialties', 'SpecialtyController@store'); // envío del form
+    Route::put('/specialties/{specialty}', 'SpecialtyController@update');
+    Route::delete('/specialties/{specialty}', 'SpecialtyController@destroy');
+    // Doctors
+    Route::resource('doctors', 'DoctorController');
+    // Patients
+    Route::resource('patients', 'PatientController');
 
-	// Charts
-	Route::get('/charts/appointments/line', 'ChartController@appointments');
-	Route::get('/charts/doctors/column', 'ChartController@doctors');
-	Route::get('/charts/doctors/column/data', 'ChartController@doctorsJson');
+    // Charts
+    Route::get('/charts/appointments/line', 'ChartController@appointments');
+    Route::get('/charts/doctors/column', 'ChartController@doctors');
+    Route::get('/charts/doctors/column/data', 'ChartController@doctorsJson');
 
-	// FCM
-	Route::post('/fcm/send', 'FirebaseController@sendAll');
+    // FCM
+    Route::post('/fcm/send', 'FirebaseController@sendAll');
 });
 
 Route::middleware(['auth', 'doctor'])->namespace('Doctor')->group(function () {
@@ -36,19 +39,19 @@ Route::middleware(['auth', 'doctor'])->namespace('Doctor')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-	Route::get('/appointments/create', 'AppointmentController@create');
-	Route::post('/appointments', 'AppointmentController@store');
+    Route::get('/appointments/create', 'AppointmentController@create');
+    Route::post('/appointments', 'AppointmentController@store');
 
-	/*
-	/appointments -> Verificar 
+    /*
+	/appointments -> Verificar
 	-> qué variables pasar a la vista
 	-> 1 único blade (condiciones)
 	*/
-	Route::get('/appointments', 'AppointmentController@index');	
-	Route::get('/appointments/{appointment}', 'AppointmentController@show');	
+    Route::get('/appointments', 'AppointmentController@index');
+    Route::get('/appointments/{appointment}', 'AppointmentController@show');
 
-	Route::get('/appointments/{appointment}/cancel', 'AppointmentController@showCancelForm');
-	Route::post('/appointments/{appointment}/cancel', 'AppointmentController@postCancel');
+    Route::get('/appointments/{appointment}/cancel', 'AppointmentController@showCancelForm');
+    Route::post('/appointments/{appointment}/cancel', 'AppointmentController@postCancel');
 
-	Route::post('/appointments/{appointment}/confirm', 'AppointmentController@postConfirm');
+    Route::post('/appointments/{appointment}/confirm', 'AppointmentController@postConfirm');
 });
